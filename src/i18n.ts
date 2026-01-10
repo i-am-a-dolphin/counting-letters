b7996e5152b5501d03a10231abd7dc7cbc8f0863
grasslandscholar <euikim.biz@gmail.com>
2026-01-11 02:25:47 +0900

    Update i18n.ts
    jp added

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import enTranslations from "./locales/en.json";
 import koTranslations from "./locales/ko.json";
 import zhTranslations from "./locales/zh.json";
+import zhTranslations from "./locales/jp.json";
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -11,6 +12,7 @@ const resources = {
   ko: { translation: koTranslations },
   en: { translation: enTranslations },
   zh: { translation: zhTranslations },
+  jp: { translation: jpTranslations },
 };
 
 i18n.use(initReactI18next).init({
